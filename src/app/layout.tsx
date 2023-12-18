94a945ac8da8629a09bba17d57c9b3887e6614dd
hzz <60465723+hzz888@users.noreply.github.com>
2023-12-18 23:28:02 +1100

    Finish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Zachary Huo',
+  title: 'Zhengzuo Huo',
   description: 'Zhengzuo Huo\'s personal website',
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,67 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { IoIosDocument } from "react-icons/io";
+
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen min-w-screen  flex-col items-center justify-start px-12 py-4 bg-black h-screen w-screen">
-      <nav className="flex flex-row items-center justify-between w-full max-w-full max-h-[15vh] border-b-2 pb-4 px-2">
-        <span className='text-2xl text-white handwritten'>Zachary Huo</span>
-        <a className='nav-item' href='https://www.linkedin.com/in/zhengzuo-huo-8ba5a4247/'>LinkedIn</a>
-      </nav>
-      
-      <div className='flex flex-col w-full items-center mt-6'>
-        <div className='flex flex-row justify-center items-center'>
-            <div className='text-6xl text-white p-2 mb-1'>👋 Hi, my name is </div>
-            <div className='text-6xl hero p-2 mb-1'>Zhengzuo Huo</div>
-        </div>
-        <div className='text-xl text-white break-words'>You can also call me Zachary. I am a software developer in Sydney, Australia.</div>
-      </div>
-      
-    </main>
-  )
+
+    return (
+        <main className="flex min-h-screen min-w-screen flex-col items-center justify-start px-12 py-4 bg-black h-screen w-screen">
+            <nav
+                className="flex flex-row items-center justify-around max-w-full w-full max-h-[15vh] border-b-2 py-4 px-2"
+            >
+                <span className="text-2xl text-white handwritten">
+                    Zhengzuo Huo
+                </span>
+                <a
+                    className="nav-item flex items-center"
+                    href="/files/Zhengzuo_Huo_Resume.pdf"
+                >
+                    <IoIosDocument />
+                    <span className="ml-1">Resume</span>
+                </a>
+                <a
+                    className="nav-item flex items-center"
+                    href="https://www.linkedin.com/in/zhengzuo-huo-8ba5a4247/"
+                >
+                    <FaLinkedin />
+                    <span className="ml-1">LinkedIn</span>
+                </a>
+                <a
+                    className="nav-item flex items-center"
+                    href="https://github.com/hzz888"
+                >
+                    <FaGithub />
+                    <span className="ml-1">Github</span>
+                </a>
+            </nav>
+
+            <div className="flex flex-col w-full items-center justify-center h-full max-h-full">
+                <div className="flex flex-row justify-center items-center mb-8">
+                    <div className="text-6xl text-white p-2 mb-1">
+                        👋 Hi, my name is{" "}
+                    </div>
+                    <div className="text-6xl hero p-2 mb-1">Zhengzuo Huo</div>
+                </div>
+                <div className="text-xl text-white break-words mb-4">
+                    You can also call me{" "}
+                    <span className="hero font-extrabold">Zachary Huo</span>. I
+                    am a software developer in Sydney, Australia.
+                </div>
+                <div className="text-xl text-white break-words mb-4">
+                    I'm currently a postgraduate student of Master of Computer
+                    Science in The University of Sydney.
+                </div>
+                <div className="text-xl text-white break-words mb-4">
+                    I'm pursuing being a full stack developer and looking
+                    forward to start my IT career. 💪
+                </div>
+                <div className="text-xl text-white break-words mb-4">
+                    For more details about me, please check my resume.
+                </div>
+                <div className="text-xl text-white break-words">
+                    Thank you! 🙌
+                </div>
+            </div>
+        </main>
+    );
 }
