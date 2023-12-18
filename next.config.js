345f990b3b4d24ab0eb94d93cd6d3dce99d68c83
hzz <60465723+hzz888@users.noreply.github.com>
2023-12-18 20:04:48 +1100

    revert

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-
-    /**
-     * Set base path. This is usually the slug of your repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/nextjs-github-pages",
-
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-     */
-    images: {
-        unoptimized: true,
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
